1f5d70e0fe237db6a8b6a313db4a25ec6bd00a35
jkpenner <jkpenner@live.com>
2023-11-13 10:59:20 -0800

    Add example Impact Game Event and related Signal

--- Game/GameEvents.cs
+++ Game/GameEvents.cs
@@ -2,6 +2,9 @@ namespace SpaceEngineer
 {
     public static class GameEvents
     {
-        
+        /// <summary>
+        /// Invoked when an inpact ship event occurs.
+        /// </summary>
+        public static readonly GameEvent Impact = new GameEvent();
     }
 }
--- Game/GameSignals.cs
+++ Game/GameSignals.cs
@@ -1,9 +1,25 @@
+using System;
 using Godot;
 
 namespace SpaceEngineer
 {
     public partial class GameSignals : Node
     {
-        
+        [Signal] public delegate void ImpactEventHandler();
+
+        public override void _EnterTree()
+        {
+            GameEvents.Impact.Connect(OnImpact);
+        }
+
+        public override void _ExitTree()
+        {
+            GameEvents.Impact.Disconnect(OnImpact);   
+        }
+
+        private void OnImpact()
+        {
+            EmitSignal(SignalName.Impact);
+        }
     }
 }
