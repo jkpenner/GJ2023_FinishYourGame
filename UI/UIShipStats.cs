d7e0d81e1462a60010458564e83e440ec6dd2dd7
Jacob Penner <jkpenner@live.com>
2023-11-17 17:21:20 -0800

    Update scenes with treadmills and new stats ui

--- UI/UIShipStats.cs
+++ UI/UIShipStats.cs
@@ -1,14 +1,11 @@
 using System;
+using System.Text;
 using Godot;
 
 namespace SpaceEngineer
 {
-    public partial class UIShipStats : Control
+    public partial class UIShipStats : Label
     {
-        [Export] Label energyLabel;
-        [Export] Label overloadStateLabel;
-        [Export] Label overloadTimeLabel;
-
         private GameManager gameManager;
 
         private ShipController Ship => gameManager?.PlayerShip;
@@ -16,58 +13,51 @@ namespace SpaceEngineer
         public override void _Ready()
         {
             this.TryGetGameManager(out gameManager);
+            UpdateText();
         }
 
         public override void _EnterTree()
         {
-            GameEvents.ShipEnergyCapacityChanged.Connect(OnShipEnergyChanged);
-            GameEvents.ShipEnergyUsageChanged.Connect(OnShipEnergyChanged);
-            GameEvents.ShipEnergyOverloading.Connect(OnShipOverloadStateChanged);
-            GameEvents.ShipEnergyOverloaded.Connect(OnShipOverloadStateChanged);
-            GameEvents.ShipEnergyNormalized.Connect(OnShipOverloadStateChanged);
+            GameEvents.ShipEnergyCapacityChanged.Connect(UpdateText);
+            GameEvents.ShipEnergyUsageChanged.Connect(UpdateText);
+            GameEvents.ShipEnergyOverloading.Connect(UpdateText);
+            GameEvents.ShipEnergyOverloaded.Connect(UpdateText);
+            GameEvents.ShipEnergyNormalized.Connect(UpdateText);
         }
 
         public override void _ExitTree()
         {
-            GameEvents.ShipEnergyCapacityChanged.Disconnect(OnShipEnergyChanged);
-            GameEvents.ShipEnergyUsageChanged.Disconnect(OnShipEnergyChanged);
-            GameEvents.ShipEnergyOverloading.Disconnect(OnShipOverloadStateChanged);
-            GameEvents.ShipEnergyOverloaded.Disconnect(OnShipOverloadStateChanged);
-            GameEvents.ShipEnergyNormalized.Disconnect(OnShipOverloadStateChanged);
+            GameEvents.ShipEnergyCapacityChanged.Disconnect(UpdateText);
+            GameEvents.ShipEnergyUsageChanged.Disconnect(UpdateText);
+            GameEvents.ShipEnergyOverloading.Disconnect(UpdateText);
+            GameEvents.ShipEnergyOverloaded.Disconnect(UpdateText);
+            GameEvents.ShipEnergyNormalized.Disconnect(UpdateText);
         }
 
         public override void _Process(double delta)
         {
-            if (Ship is not null && Ship.OverloadState == ShipOverloadState.Overloading)
-            {
-                overloadTimeLabel.Text = $"Time till overload: {Ship.GetRemainingTimeTillOverload()}";
-            }
-            else
-            {
-                overloadTimeLabel.Text = "Time till overload: N/a";
-            }
+            UpdateText();
         }
 
-        private void OnShipEnergyChanged(int _)
+        private void UpdateText(int _) => UpdateText();
+
+        private void UpdateText()
         {
-            if (Ship is null)
-            {
-                energyLabel.Text = "Energy Usage: Unknown, Energy Capacity: Unknown";
-                return;
-            }
-
-            energyLabel.Text = $"Energy Usage: {Ship.EnergyUsage}, Energy Capacity: {Ship.EnergyCapacity}";
-        }
-
-        private void OnShipOverloadStateChanged()
-        {
-            if (Ship is null)
-            {
-                overloadStateLabel.Text = "Overload State: Unknown";
-                return;
-            }
-
-            overloadStateLabel.Text = $"Overload State: {Ship.OverloadState}";
+            var builder = new StringBuilder();
+            builder.Append("Energy\n");
+            builder.Append($"Usage: {Ship.EnergyUsage}\n");
+            builder.Append($"Capacity: {Ship.EnergyCapacity}\n");
+            builder.Append($"Max Energy: {Ship.MaximumEnergy}\n");
+            builder.Append($"Regen Rate: {Ship.GetEnergyRegenRate()}\n");
+            builder.Append("\nOverload\n");
+            builder.Append($"State: {Ship.OverloadState}\n");
+            builder.Append($"Time Till Overload: {Ship.GetRemainingTimeTillOverload()}\n");
+            builder.Append("\nSystems\n");
+            builder.Append($"Weapons: {Ship.WeaponSystem.State}\n");
+            builder.Append($"Shields: {Ship.ShieldSystem.State}\n");
+            builder.Append($"Engines: {Ship.EngineSystem.State}\n");
+            builder.Append($"Sensors: {Ship.SensorSystem.State}\n");
+            Text = builder.ToString();
         }
     }
 }
