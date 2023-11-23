b4b59db34a36259faee5f19fe071d8756c6a6811
Jacob Penner <jkpenner@live.com>
2023-11-22 16:02:47 -0800

    Update the ui weapon status elements and place in the UI scene

--- UI/Systems/UIWeaponStatus.cs
+++ UI/Systems/UIWeaponStatus.cs
@@ -6,47 +6,62 @@ namespace SpaceEngineer
     public partial class UIWeaponStatus : Control
     {
         private const string SYSTEM_NAME_NODE_PATH = "WeaponState/MarginContainer/HBoxContainer/WeaponName";
-        private const string STATE_CONTAINER_PATH = "SystemPower/MarginContainer/HBoxContainer/PanelContainer";
+        private const string STATE_CONTAINER_PATH = "WeaponState/MarginContainer/HBoxContainer/PanelContainer";
         private const string STATE_NODE_PATH = "WeaponState/MarginContainer/HBoxContainer/PanelContainer/MarginContainer/WeapnState";
-        private const string STATUS_CONTAINER_PATH = "Status";
-        private const string STATUS_LABEL_PATH = "Status/MarginContainer/HBoxContainer/Status";
+        private const string STATUS_PROGRESS_PATH = "Status/ProgressBar";
+        private const string STATUS_LABEL_PATH = "Status/Status";
 
         [Export] Color loaded = new Color("#00FF00");
         [Export] Color empty = new Color("#FF0000");
 
-        [Export] Color firing = new Color("#AA0000");
+        [Export] Color firing = new Color("#ff4800");
 
         private GameManager gameManager;
         private Weapon weapon;
 
         private Label weaponName;
-        private Control stateContainer;
+        private PanelContainer stateContainer;
         private Label state;
-        private Control statusContainer;
-        private Label status;
+
+        private ProgressBar statusProgress;
+        private Label statusLabel;
 
         public override void _Ready()
         {
             this.TryGetGameManager(out gameManager);
 
             weaponName = GetNode<Label>(SYSTEM_NAME_NODE_PATH);
-            stateContainer = GetNode<Control>(STATE_CONTAINER_PATH);
+            stateContainer = GetNode<PanelContainer>(STATE_CONTAINER_PATH);
             state = GetNode<Label>(STATE_NODE_PATH);
-            statusContainer = GetNode<Control>(STATUS_CONTAINER_PATH);
-            status = GetNode<Label>(STATUS_LABEL_PATH);
+            statusProgress = GetNode<ProgressBar>(STATUS_PROGRESS_PATH);
+            statusLabel = GetNode<Label>(STATUS_LABEL_PATH);
+
+            OnItemChanged(null);
         }
 
         public override void _Process(double delta)
         {
             if (gameManager.PlayerShip.ActiveWeapon == weapon)
             {
-                statusContainer.Visible = true;
-                statusContainer.SelfModulate = firing;
-                status.Text = "Firing";
+                if (gameManager.PlayerShip.CombatState == ShipCombatState.Targeting)
+                {
+                    statusProgress.Visible = true;
+                    statusProgress.SelfModulate = firing;
+                    statusProgress.Value = gameManager.PlayerShip.WeaponTargetingPercent;
+                    
+                    statusLabel.Visible = true;
+                    statusLabel.Text = "Firing";
+                }
+                else
+                {
+                    statusProgress.Visible = false;
+                    statusLabel.Visible = false;
+                }
             }
             else
             {
-                statusContainer.Visible = false;
+                statusProgress.Visible = false;
+                statusLabel.Visible = false;
             }
         }
 
